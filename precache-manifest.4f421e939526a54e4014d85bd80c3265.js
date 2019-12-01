e030e0c2f663ef28b25bc5ce2a609b0a2649c65f
Felipe Freitas <felipe.samidare@hotmail.com>
2019-12-01 06:39:33 -0300

    Updates

---
+++ precache-manifest.4f421e939526a54e4014d85bd80c3265.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e1d7a5f6d59d7e899ac08e2f1bcda93",
+    "url": "/desafio-front-end/index.html"
+  },
+  {
+    "revision": "ebf39356f0b6ce10ce3e",
+    "url": "/desafio-front-end/static/css/main.47bca470.chunk.css"
+  },
+  {
+    "revision": "00440d17ba3edff6e16f",
+    "url": "/desafio-front-end/static/js/2.c0b0beed.chunk.js"
+  },
+  {
+    "revision": "ebf39356f0b6ce10ce3e",
+    "url": "/desafio-front-end/static/js/main.e30e7b89.chunk.js"
+  },
+  {
+    "revision": "ba0b51a314c67478b7b4",
+    "url": "/desafio-front-end/static/js/runtime-main.d3466b3f.js"
+  }
+]);
